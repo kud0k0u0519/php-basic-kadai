85c6a94764550e2a2a88e2b7eea8518f4e438ce8
kud0k0u0519 <kud0k0u.5519@gmail.com>
2024-10-01 00:17:46 +0900

    課題データのアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang=" ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+      <?php
+      $goods_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      echo '<br>';
+
+      print_r ($goods_data);
+      ?>
+  </p>
+</body>
+
+</html>
